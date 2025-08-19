963fcc93845da1880805f5ff67247793258fb143
Yasssh-bombale <yashbombale@gmail.com>
2025-08-19 19:00:18 +0530

    add cv data

--- cv-data.js
+++ cv-data.js
@@ -1,33 +1,243 @@
 export const cvData = {
-  name: "Yash Bombale",
-  email: "yashbombale@gmail.com",
-  phone: "1234567890",
-  address: "123 Main St, Anytown, USA",
-  education: [
-    {
-      school: "University of Example",
-      degree: "Bachelor of Science in Computer Science",
-      gpa: "4.0",
-      startDate: "2015-08-01",
-      endDate: "2019-05-31",
+  personalDetails: {
+    name: "yashbombale",
+    email: "yashbombale@gmail.com",
+    location: "lucknow",
+    profession: "employee",
+    imageUrl:
+      "https://trucvstorage.blob.core.windows.net/uploads/9d8054ea94984b7aa3b055a56d03882db783a0c571456a79dcdd83727cd63597_1755607561.jpeg",
+    phoneNumber: "9988998877",
+    linkedinProfile: "https://linkdein.com",
+    twitterProfile: "https://x.com",
+    telegramProfile: "https://linkdein.com",
+    instagramProfile: "https://linkdein.com",
+    githubProfile: "https://linkdein.com",
+    years_of_experience: "2",
+  },
+  education: {
+    underGraduateDuration: {
+      duration: {
+        from: "Aug 06 2025",
+        to: "Aug 14 2025",
+      },
     },
-  ],
-  workExperience: [
+    postGraduateDuration: {
+      duration: {
+        from: "Date",
+        to: "Date",
+      },
+    },
+    class10School: "LBTVM",
+    class10Board: "ICSE",
+    class10Grade: 90,
+    class12College: "Bhimashankar padalkar college",
+    class12Board: "ICSE",
+    class12Grade: 86,
+    underGraduateCollege: "CSMSS",
+    underGraduateDegree: "Btech",
+    underGraduateGPA: 9,
+  },
+  achievements: {
+    awards: [
+      {
+        award_name: "award1",
+        awarding_organization: "edubuk demo organization",
+        date_of_achievement: "Aug 05 2025",
+        description:
+          "A brief description of the award or achievement and its significance.",
+        _id: "68a4736ec6db74c1ebd77ccf",
+      },
+      {
+        award_name: "award 2",
+        awarding_organization: "award2 organization",
+        date_of_achievement: "Aug 07 2025",
+        description:
+          "A brief description of the award or achievement and its significance.",
+        _id: "68a4736ec6db74c1ebd77cd0",
+      },
+    ],
+    courses: [
+      {
+        duration: {
+          from: "Aug 05 2025",
+          to: "Aug 08 2025",
+        },
+        course_name: "course 1",
+        organization: "sfdgdsgdsg",
+        description:
+          "A brief description of the award or achievement and its significance.",
+        _id: "68a4736ec6db74c1ebd77cd1",
+      },
+    ],
+    projects: [
+      {
+        duration: {
+          from: "Jul 08 2025",
+          to: "Aug 05 2025",
+        },
+        project_name: "project1",
+        project_url: "project1.com",
+        description:
+          "A brief description of the award or achievement and its significance.",
+        _id: "68a4736ec6db74c1ebd77cd2",
+      },
+    ],
+  },
+  personalDetailsVerification: {
+    name: {
+      isSelfAttested: true,
+    },
+    email: {
+      isSelfAttested: true,
+    },
+    location: {
+      isSelfAttested: true,
+    },
+    profession: {
+      isSelfAttested: true,
+    },
+    imageUrl: {
+      isSelfAttested: true,
+    },
+    phoneNumber: {
+      isSelfAttested: true,
+    },
+  },
+  educationVerifications: {
+    class10: {
+      isSelfAttested: true,
+      proof: [],
+    },
+    class12: {
+      isSelfAttested: true,
+      proof: [],
+    },
+    undergraduation: {
+      isSelfAttested: true,
+      proof: [],
+    },
+    postgraduation: {
+      isSelfAttested: false,
+      proof: [],
+    },
+  },
+  profileSummaryVerification: {
+    profile_summary: {
+      isSelfAttested: false,
+    },
+  },
+  _id: "68a4736ec6db74c1ebd77cc8",
+  nanoId: "yashbombale-CV-dsRUZTuIK0n-MOeN",
+  experience: [
     {
-      company: "Example Company",
-      title: "Software Engineer",
-      startDate: "2019-06-01",
-      endDate: "2021-05-31",
+      duration: {
+        from: "Nov 01 2024",
+        to: "Jun 10 2025",
+      },
+      company_name: "AAYAAM AI",
       description:
-        "Developed and maintained software applications for clients.",
+        "worked on full stack platform of aayaamai , build candidate ai ranking portal",
+      job_role: "gen ai full stack",
+      experienceCertUrl: "",
+      _id: "68a4736ec6db74c1ebd77cc9",
+    },
+    {
+      duration: {
+        from: "Jun 10 2025",
+        to: "present",
+      },
+      company_name: "EDUBUK",
+      description: "worked on trujjobs, miit ",
+      job_role: "gen ai full stack",
+      experienceCertUrl: "",
+      _id: "68a4736ec6db74c1ebd77cca",
     },
   ],
   skills: [
-    "JavaScript",
-    "Python",
-    "Node.js",
-    "React.js",
-    "Express.js",
-    "MongoDB",
+    {
+      skillName: "Project management",
+      skillUrl: "",
+      _id: "68a4736ec6db74c1ebd77ccb",
+    },
+    {
+      skillName: "Problem solving",
+      skillUrl: "",
+      _id: "68a4736ec6db74c1ebd77ccc",
+    },
+    {
+      skillName: "Software proficiency",
+      skillUrl: "",
+      _id: "68a4736ec6db74c1ebd77ccd",
+    },
+    {
+      skillName: "Adaptability",
+      skillUrl: "",
+      _id: "68a4736ec6db74c1ebd77cce",
+    },
   ],
+  profile_summary:
+    "Provide a brief overview of professional background, key skills, and accomplishments. Highlight expertise, experience, and unique strengths in 2-3 sentences.",
+  experienceVerifications: {
+    "AAYAAM AI": {
+      isSelfAttested: true,
+      proof: [],
+      _id: "68a4736ec6db74c1ebd77cd3",
+    },
+    EDUBUK: {
+      isSelfAttested: true,
+      proof: [],
+      _id: "68a4736ec6db74c1ebd77cd4",
+    },
+  },
+  skillsVerifications: {
+    "Project management": {
+      isSelfAttested: true,
+      proof: [],
+      _id: "68a4736ec6db74c1ebd77cd5",
+    },
+    "Problem solving": {
+      isSelfAttested: true,
+      proof: [],
+      _id: "68a4736ec6db74c1ebd77cd6",
+    },
+    "Software proficiency": {
+      isSelfAttested: true,
+      proof: [],
+      _id: "68a4736ec6db74c1ebd77cd7",
+    },
+    Adaptability: {
+      isSelfAttested: true,
+      proof: [],
+      _id: "68a4736ec6db74c1ebd77cd8",
+    },
+  },
+  awardVerifications: {
+    award1: {
+      isSelfAttested: false,
+      proof: [""],
+      _id: "68a4736ec6db74c1ebd77cd9",
+    },
+    "award 2": {
+      isSelfAttested: true,
+      proof: [""],
+      _id: "68a4736ec6db74c1ebd77cda",
+    },
+  },
+  courseVerifications: {
+    "course 1": {
+      isSelfAttested: true,
+      proof: [""],
+      _id: "68a4736ec6db74c1ebd77cdb",
+    },
+  },
+  projectsVerifications: {
+    project1: {
+      isSelfAttested: true,
+      proof: [""],
+      _id: "68a4736ec6db74c1ebd77cdc",
+    },
+  },
+  createdAt: "2025-08-19T12:51:58.195Z",
+  updatedAt: "2025-08-19T12:51:58.195Z",
+  __v: 0,
 };
